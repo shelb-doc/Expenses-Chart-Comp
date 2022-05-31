0b1f3a0c714fe7d35fb7e7cf12a2b01053b5501e
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2022-05-31 10:23:12 -0400

    fixed js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,87 @@
-import '../css/style.css'
-import data from '../resources/data.json'
+const labels = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-const days = document
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "",
+      borderRadius: 5,
+      backgroundColor: [
+        "hsl(10, 79%, 65%)",
+        "hsl(10, 79%, 65%)",
+        "hsl(186, 34%, 60%)",
+        "hsl(10, 79%, 65%)",
+        "hsl(10, 79%, 65%)",
+        "hsl(10, 79%, 65%)",
+      ],
+      data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+    },
+  ],
+};
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {
+    animation: {
+      duration: 3000,
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+        ticks: {
+          font: {
+            family: "DM Sans", // Your font family
+            size: 14,
+          },
+          color: "hsl(28, 10%, 53%)",
+        },
+      },
+      y: {
+        display: false,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          title: () => null,
+          label: function (context) {
+            let label = context.dataset.label || "";
+
+            if (label) {
+              label += ": ";
+            }
+            if (context.parsed.y !== null) {
+              label += new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(context.parsed.y);
+            }
+            return label;
+          },
+        },
+      },
+    },
+  },
+};
+Chart.defaults.font.family = "DM Sans";
+Chart.defaults.plugins.tooltip.padding = 10;
+// Chart.defaults.plugins.tooltip.bodyFont.size = 100
+Chart.defaults.plugins.tooltip.displayColors = false;
+Chart.defaults.plugins.tooltip.backgroundColor = "hsl(25, 47%, 15%)";
+Chart.defaults.plugins.tooltip.cornerRaduis = 5;
+Chart.defaults.plugins.tooltip.xAlign = "center";
+Chart.defaults.plugins.tooltip.yAlign = "bottom";
+Chart.defaults.plugins.tooltip.bodyAlign = "center";
+Chart.defaults.plugins.tooltip.titleAlign = "center";
+
+const myChart = new Chart(
+  document.getElementById("myChart").getContext("2d"),
+  config
+);
